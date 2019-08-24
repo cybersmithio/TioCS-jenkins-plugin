c1719944c053dfe54ef99bc7ecc05ed01dc9b755
James <james@quizlicious.com>
2019-08-24 17:17:58 -0400

    Trying to parse JSON string into JSON object.

--- src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
+++ src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
@@ -175,7 +175,7 @@ public class TioCSBuilder extends Builder implements SimpleBuildStep {
 
             listener.getLogger().println("Retrieving report of image " + name + " from Tenable.io API");
             String jsonstring="";
-            Int c=0
+            Int c=0;
             try {
                 URL myUrl = new URL("https://cloud.tenable.com/container-security/api/v2/reports/"+TioRepo+"/"+name+"/latest");
                 HttpsURLConnection conn = (HttpsURLConnection)myUrl.openConnection();
@@ -191,7 +191,7 @@ public class TioCSBuilder extends Builder implements SimpleBuildStep {
                 while ((inputLine = br.readLine()) != null) {
                     listener.getLogger().println(c+ ">>>"+inputLine);
                     jsonstring.concat(inputLine);
-                    c = c + 1
+                    c = c + 1;
                 }
 
                 br.close();
