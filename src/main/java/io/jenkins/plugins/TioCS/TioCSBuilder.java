2aa8440f002f57dd14fa9d7bfe3155027558772e
James <james@quizlicious.com>
2019-08-24 15:34:22 -0400

    Had run time issues with external process.  Updated the calling syntax.

--- src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
+++ src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
@@ -105,8 +105,8 @@ public class TioCSBuilder extends Builder implements SimpleBuildStep {
             listener.getLogger().println("Logging into registry.cloud.tenable.com with username " + TioUsername );
             ProcessBuilder processBuilder = new ProcessBuilder();
             try {
-                Process process=processBuilder.start();
-                processBuilder.command("bash", "-c", "ls");
+                Process process=new ProcessBuilder.("bash", "-c", "ls").start();
+
                 StringBuilder output = new StringBuilder();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                 String line;
