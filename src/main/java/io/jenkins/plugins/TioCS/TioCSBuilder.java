9b784169aa65c0d5682a6f45cbe4f1f563c0df8e
James <james@quizlicious.com>
2019-08-24 22:35:22 -0400

    Added failure if image has vulns higher than FailCVSS

--- src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
+++ src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
@@ -238,7 +238,7 @@ public class TioCSBuilder extends Builder implements SimpleBuildStep {
             if (Double.compare(highcvss,FailCVSS) >= 0 ) {
                 listener.getLogger().println("There are vulnerabilities equal to or higher than "+highcvss);
                 listener.getLogger().println("Failing this build!");
-                exit(1);
+                System.exit(1);
             } else {
                 listener.getLogger().println("Vulnerabilities are below threshold of "+highcvss);
             }
