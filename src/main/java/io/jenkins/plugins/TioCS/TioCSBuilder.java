efcbf1cea7dabfc909cd82e2075817359c6df61e
James <james@quizlicious.com>
2019-08-29 22:10:53 -0400

    Merge all fields related to Container Security into one section of the plugin, in anticipation of VM and WAS.

--- src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
+++ src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
@@ -582,7 +582,7 @@ public class TioCSBuilder extends Builder implements SimpleBuildStep {
 
         public ListBoxModel doFillScanFolderItems() {
             ListBoxModel items = new ListBoxModel();
-            if (getTioAccessKey().length() != 0 && getTioSecretKey().length() != 0) {
+            if (this.getTioAccessKey().length() != 0 && getTioSecretKey().length() != 0) {
                 //TODO Change this to query Tenable.io for the scan names
                 items.add("1","Test Folder 1");
                 items.add("2","Test Folder 2");
