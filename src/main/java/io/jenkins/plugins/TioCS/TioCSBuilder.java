bdec458408f31f96369d4f7fb41d53b2068d6d84
James <james@quizlicious.com>
2019-09-13 12:57:23 -0400

    Working on credentials integration

--- src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
+++ src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
@@ -5,6 +5,7 @@ import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import hudson.model.Item;
 import org.kohsuke.stapler.AncestorInPath;
+import jenkins.model.Jenkins;
 
 //Needed for Map class, when looking for all environment variables.
 import java.util.HashMap;
