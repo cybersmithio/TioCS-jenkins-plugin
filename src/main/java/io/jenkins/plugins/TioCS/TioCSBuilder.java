7006d678be124bf08bf56f9695bf9480064f7fd3
James <james@quizlicious.com>
2019-09-13 14:43:56 -0400

    Still working on integration with credentials...

--- src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
+++ src/main/java/io/jenkins/plugins/TioCS/TioCSBuilder.java
@@ -12,6 +12,7 @@ import com.cloudbees.plugins.credentials.common.StandardCredentials;
 import com.cloudbees.plugins.credentials.domains.DomainRequirement;
 import hudson.security.ACL;
 import java.util.List;
+import static com.google.common.collect.Lists.newArrayList;
 
 //Needed for Map class, when looking for all environment variables.
 import java.util.HashMap;
